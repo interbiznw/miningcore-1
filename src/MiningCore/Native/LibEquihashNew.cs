e412d0b3005170e47237a14527587103e23f9be1
J62 <j.owens@interbiznw.com>
2018-04-24 14:17:50 -0700

    Update LibEquihashNew.cs

--- src/MiningCore/Native/LibEquihashNew.cs
+++ src/MiningCore/Native/LibEquihashNew.cs
@@ -5,7 +5,7 @@ namespace MiningCore.Native
 {
     public static unsafe class LibEquihashVerifyNew
     {
-        [DllImport("libequihashverifynew", EntryPoint = "equihash_verify_new_export", Callingconvention = Callingconvention.CDecl)]
+        [DllImport("libequihashverifynew", EntryPoint = "equihash_verify_new_export", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool equihash_verify_new(byte* header, int header_length, byte* solution, int solution_length);
     }
 }
